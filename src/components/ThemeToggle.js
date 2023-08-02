812b70c219e16e39d99f9b8ca642fcbb10487964
Carly Gettinger <cjgettinger@gmail.com>
2023-08-02 14:54:28 +0200

    fancify theme toggle

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { Switch } from "@material-ui/core";
 import { useThemeContext } from "../context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 export const ThemeToggle = () => {
   const { colorMode, toggleColorMode } = useThemeContext();
 
+
   return (
-    <Switch
-      checked={colorMode === "dark"}
-      onChange={toggleColorMode}
-      color="secondary"
-    />
+    <div style={{ position: 'relative', display: 'inline-block' }}>
+      <Switch
+        checked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        color="secondary"
+      />
+      <FontAwesomeIcon
+        icon={colorMode==="dark" ? faMoon : faSun}
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: colorMode==="dark" ? '68%' : '32%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 1,
+          pointerEvents:'none',
+        }}
+      />
+    </div>
   );
 };
